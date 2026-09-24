0967e9e9707cfa6461ccb08b30850a67e07cdc2a
dosantosdev <dosantosdev@gmail.com>
2026-09-24 18:09:38 -0300

    Add Accordion and AccordionItem components with initial implementation in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import Accordion from "./components/Accordion/Accordion.jsx";
+import AccordionItem from "./components/Accordion/AccordionItem.jsx";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2>Why work with us?</h2>
+        <Accordion className="accordion">
+          <AccordionItem title="We got 20 years of expirience">
+            <article>
+              <p>You can&apos;t go wrong with us.</p>
+              <p>
+                We are in the business of planning highly individualized
+                vacation trips for more than 20 years.
+              </p>
+            </article>
+          </AccordionItem>
+          <AccordionItem title="We working with local guides">
+            <article>
+              <p>We are not doing this along from our office.</p>
+              <p>
+                Instead, we are working with a local guides to ensure a safe and
+                pleasant vacation.
+              </p>
+            </article>
+          </AccordionItem>
+        </Accordion>
+      </section>
+    </main>
+  );
 }
 
 export default App;

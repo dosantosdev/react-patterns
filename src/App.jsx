01a8e60b0ebf7550ab6f22bb93709468903eeae8
dosantosdev <dosantosdev@gmail.com>
2026-09-25 08:52:33 -0300

    Implement Accordion context and enhance AccordionItem with open/close functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,11 @@ function App() {
       <section>
         <h2>Why work with us?</h2>
         <Accordion className="accordion">
-          <AccordionItem title="We got 20 years of expirience">
+          <AccordionItem
+            id="experience"
+            className="accordion-item"
+            title="We got 20 years of expirience"
+          >
             <article>
               <p>You can&apos;t go wrong with us.</p>
               <p>
@@ -16,7 +20,11 @@ function App() {
               </p>
             </article>
           </AccordionItem>
-          <AccordionItem title="We working with local guides">
+          <AccordionItem
+            id="local-guides"
+            className="accordion-item"
+            title="We working with local guides"
+          >
             <article>
               <p>We are not doing this along from our office.</p>
               <p>
